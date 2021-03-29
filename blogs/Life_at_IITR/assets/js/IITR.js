f7ea52de39acc9e1154fdfa4ffc85e9275cc2024
Yagyaansh Khaneja <yagyaansh@github.com>
2021-03-29 06:12:48 +0530

    carousel css only
    and also gordita regular font changes, see what happens

--- blogs/Life_at_IITR/assets/js/IITR.js
+++ blogs/Life_at_IITR/assets/js/IITR.js
@@ -12,4 +12,24 @@ function switchTheme(){
     }
 }
 
+// not required below this ----------------------------------------------
+
 function addComment(){}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
